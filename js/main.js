464ae684866a78f3fb434f29127c4f3517166ea1
Gustavo <gs6923698@email.com>
2026-04-03 01:00:56 -0300

    criacao de item da box quase completo

--- js/main.js
+++ js/main.js
@@ -1,17 +1,68 @@
 let btnAdd = document.getElementById('btn-add-item');
 
+
+
+let boxItem = document.querySelector('.box-items');
+
 btnAdd.addEventListener('click', ()=>{
-    criarCard();
+    const info = getInfo();
+    criarCard(info);
 })
 
-function criarCard(){
+function getInfo(){
+    const inputImg = document.getElementById('add-img');
+    const arquivo = inputImg.files[0]; // era 'arquive'
+    const imgURL = arquivo ? URL.createObjectURL(arquivo) : null;
+
+    const inputName = document.getElementById('add-name');
+    const inputValue = document.getElementById('add-value');
+
+    if(!arquivo){
+        alert('Adicione uma imagem');
+        return;
+    }
+
+    if(inputName.value.trim() === ''){
+        alert('Adicione um nome');
+        return;
+    }
+
+    if(inputValue.value.trim() === ''){
+        alert('Adicione um valor');
+        return;
+    }
+
+
+    const name = inputName.value;  // salva antes de limpar
+    const value = inputValue.value;
+
+    inputImg.value = '';
+    inputName.value = '';
+    inputValue.value = '';
+
+    return {
+        imgURL,
+        name,
+        value
+    }
+
+}
+
+
+function criarCard(info){
     // 1. Card corpo
     const card = document.createElement('div');
     card.classList.add('itemW');
 
     // 2. Imagem do card
     const img = document.createElement('div');
-    img.classList.add('box-pics')
+    img.classList.add('box-pics');
+
+    if (info.imgURL) {
+    const imgElement = document.createElement('img');
+    imgElement.src = info.imgURL;
+    img.appendChild(imgElement);    
+    }
 
     //3. Detalhes card
     const details = document.createElement('div');
@@ -20,10 +71,12 @@ function criarCard(){
     //4. Nome do item
     const itemName = document.createElement('p');
     itemName.classList.add('item-name');
+    itemName.textContent = info.name;
 
     //5. Valor do item
     const itemValue = document.createElement('p');
     itemValue.classList.add('item-value');
+    itemValue.textContent = `R$: ${parseFloat(info.value).toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}`;
 
     //6. Box da barra de progresso
     const progressBar = document.createElement('div');
@@ -39,24 +92,24 @@ function criarCard(){
 
     //9. btn complete
     const btnCpt = document.createElement('button');
-    btnCpt.classList.add('btn-complete');
+    btnCpt.classList.add('btn-complete', 'btn');
+    btnCpt.textContent = 'cpt';
 
-    //9. btn delete
+    //10. btn delete
     const btnDel = document.createElement('button');
-    btnDel.classList.add('btn-delete');
-}
+    btnDel.classList.add('btn-delete', 'btn');
+    btnDel.textContent = 'del';
 
-          //  <div class="itemW" id="itemId">
-           //         <div class="box-pics" id="item-photo">foto</div>
-             //       <div class="box-name-price box-mbt">
-              //          <p class="item-name">Nome</p>
-              //          <p class="item-value">valor</p>
-              //      </div>
-              //      <div class="box-progress box-mbt">
-              //          <span class="item-progress">----barra----</span>
-               //     </div>
-              //      <div class="cpt-del">
-              //          <button class="btn-complete">cpt</button>
-              //          <button class="btn-delete">del</button>
-               //     </div>
-               // </div>
+    details.appendChild(itemName);
+    details.appendChild(itemValue);
+    progressBar.appendChild(itemProgress);
+    boxBtn.appendChild(btnCpt);
+    boxBtn.appendChild(btnDel);
+
+    card.appendChild(img);
+    card.appendChild(details);
+    card.appendChild(progressBar);
+    card.appendChild(boxBtn);
+
+    boxItem.appendChild(card);
+}
